311a5b26d4f3155d3556c354440ed681f0f80e78
geon-jegal <73727480+geon-jegal@users.noreply.github.com>
2023-06-12 08:56:08 +0900

    Update gpt.c

--- gpt.c
+++ gpt.c
@@ -6,17 +6,27 @@
 #define MAX_ROWS 10
 #define MAX_COLS 10
 
-struct ModifiedData {
+struct ModifiedCompressData {
     int row;
     int col;
     int height;
-    int binaryData;
-    int asciiData;
+    int data;
 };
 
 // 변조된 위치 정보를 저장할 전역 변수
-struct ModifiedData modifiedLocations[4];
+struct ModifiedCompressData modifiedLocations[4];
 int modifiedCount = 0;
+
+struct ModifiedAsciiData {
+    int x;
+    int y;
+    const char* name;
+    int data;
+};
+
+// 변조된 위치 정보를 저장할 전역 변수
+struct ModifiedAsciiData asciiLocations[4];
+int asciiCount = 0;
 
 // 2개의 데이터를 비교하여 변조된 4곳을 특정하는 함수
 void findModifiedLocations(int data1[MAX_DIM][MAX_ROWS][MAX_COLS], int data2[MAX_DIM][MAX_ROWS][MAX_COLS]) {
@@ -27,8 +37,7 @@ void findModifiedLocations(int data1[MAX_DIM][MAX_ROWS][MAX_COLS], int data2[MAX
                     modifiedLocations[modifiedCount].row = i;
                     modifiedLocations[modifiedCount].col = j;
                     modifiedLocations[modifiedCount].height = d;
-                    modifiedLocations[modifiedCount].binaryData = data2[d][i][j];
-                    modifiedLocations[modifiedCount].asciiData = -1;  // 아스키 데이터 변조되지 않음
+                    modifiedLocations[modifiedCount].data = 0;
                     modifiedCount++;
                 }
             }
@@ -37,20 +46,33 @@ void findModifiedLocations(int data1[MAX_DIM][MAX_ROWS][MAX_COLS], int data2[MAX
 }
 
 // 아스키 데이터 변조 확인 및 전역 변수에 저장하는 함수
-void checkAndSaveModifiedAsciiData(int asciiData[MAX_DIM][MAX_ROWS][MAX_COLS]) {
-    for (int d = 0; d < MAX_DIM; d++) {
-        for (int i = 0; i < MAX_ROWS; i++) {
-            for (int j = 0; j < MAX_COLS; j++) {
-                if (asciiData[d][i][j] != modifiedLocations[d].binaryData) {
-                    modifiedLocations[modifiedCount].row = i;
-                    modifiedLocations[modifiedCount].col = j;
-                    modifiedLocations[modifiedCount].height = d;
-                    modifiedLocations[modifiedCount].binaryData = -1;  // 압축 데이터 변조되지 않음
-                    modifiedLocations[modifiedCount].asciiData = asciiData[d][i][j];
-                    modifiedCount++;
-                }
+void checkAndSaveModifiedAsciiData(int asciiData1[MAX_ROWS][MAX_COLS], int asciiData2[MAX_ROWS][MAX_COLS]) {
+    for (int i = 0; i < MAX_ROWS; i++) {
+        for (int j = 0; j < MAX_COLS; j++) {
+            if (asciiData1[i][j] != asciiData2[i][j]) {
+                asciiLocations[asciiCount].x = i;
+                asciiLocations[asciiCount].y = j;
+                asciiLocations[asciiCount].name = " ";
+                asciiLocations[asciiCount].data = 0;
+                asciiCount++;
             }
         }
+    }
+}
+
+// 변조된 위치에 대한 데이터 복원 함수
+void restoreModifiedData(int data[MAX_DIM][MAX_ROWS][MAX_COLS], struct ModifiedCompressData locations[], int count) {
+    for (int i = 0; i < count; i++) {
+        int row = locations[i].row;
+        int col = locations[i].col;
+        int height = locations[i].height;
+        int restoredData = 0;  // 복원된 데이터 값
+        // 복원 로직 구현
+        // 예를 들어, 특정 규칙에 따라 데이터를 복원한다고 가정
+        // 복원된 값을 restoredData 변수에 저장
+
+        // 복원된 데이터를 해당 위치에 저장
+        data[height][row][col] = restoredData;
     }
 }
 
@@ -58,8 +80,12 @@ int main() {
     // 예시 데이터
     int binaryData1[MAX_DIM][MAX_ROWS][MAX_COLS];
     int binaryData2[MAX_DIM][MAX_ROWS][MAX_COLS];
-    int asciiData1[MAX_DIM][MAX_ROWS][MAX_COLS];
-    int asciiData2[MAX_DIM][MAX_ROWS][MAX_COLS];
+    int asciiData1_1[MAX_ROWS][MAX_COLS];
+    int asciiData1_2[MAX_ROWS][MAX_COLS];
+    int asciiData1_3[MAX_ROWS][MAX_COLS];
+    int asciiData2_1[MAX_ROWS][MAX_COLS];
+    int asciiData2_2[MAX_ROWS][MAX_COLS];
+    int asciiData2_3[MAX_ROWS][MAX_COLS];
 
     // 바이너리 파일에서 압축 데이터와 아스키 데이터를 추출하여 binaryData1, binaryData2, asciiData1, asciiData2에 저장하는 로직
 
@@ -67,14 +93,18 @@ int main() {
     findModifiedLocations(binaryData1, binaryData2);
 
     // 아스키 데이터 변조 확인 및 저장
-    checkAndSaveModifiedAsciiData(asciiData1);
+    checkAndSaveModifiedAsciiData(asciiData1_1, asciiData2_1);
+    checkAndSaveModifiedAsciiData(asciiData1_2, asciiData2_2);
+    checkAndSaveModifiedAsciiData(asciiData1_3, asciiData2_3);
 
     // 변조된 위치 정보 출력
     for (int i = 0; i < modifiedCount; i++) {
         printf("Modified Location %d:\n", i + 1);
         printf("Row: %d, Col: %d, Height: %d\n", modifiedLocations[i].row, modifiedLocations[i].col, modifiedLocations[i].height);
-        printf("Binary Data: %d, Ascii Data: %d\n\n", modifiedLocations[i].binaryData, modifiedLocations[i].asciiData);
     }
+
+    // 변조된 위치에 대한 데이터 복원
+    restoreModifiedData(binaryData1, modifiedLocations, modifiedCount);
 
     return 0;
 }

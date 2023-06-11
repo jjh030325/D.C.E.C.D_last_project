933f466e5e3e6fa1d6ad3c949c2526c1f3417899
jjh030325 <wpqlks7@gmail.com>
2023-06-11 18:37:44 +0900

    endecode 업데이트

--- decode.c
+++ decode.c
@@ -2,29 +2,133 @@
 #include <stdlib.h>
 #include <string.h>
 
-void getFile(){
-	// 파일을 받아오는 함수, 문자열과 아스키 데이터를 추출하는 함수
-}
+int** ASC_row;
+int** ASC_column;
+int ASC_height[10][10] = { 0 };
 
-void setTable(){
-	// 파일에서 압축 문자열을 테이블로 세팅 하는 함수
-}
+int** compare_ASC_row;
+int** compare_ASC_column;
+int compare_ASC_height[10][10] = { 0 };
 
-void getTable(){
+
+void getTable(const char* inputFile) {
 	// 변조 부분을 찾기 위한 아스키 테이터를 추출 하는 함수
-}
+	FILE* output = fopen(inputFile, "rb");
+	if (output == NULL) {
+		fprintf(stderr, "열 수 없습니다.\n");
+		exit(0);
+	}
 
-void checkData(){
+
+	// 2차원 배열 할당 받기 + 마지막 줄 찾기
+	int size = 0;
+	int enter = 0;
+	char ch;
+	while (1) {
+		if (fread(&ch, 1, 1, output) != 1) break;
+		size++;
+		if (ch == '\n') {
+			enter++;
+			for (int i = 0; i < 5; i++) {
+				fread(&ch, 1, 1, output);
+				size++;
+				if (ch == '\n') enter++;
+			}
+		}
+
+		if (enter > 3) {
+			printf("GOOD\n");
+			size -= 5;
+			break;
+		}
+		enter = 0;
+	}  // 파일 전체 사이즈 + 아스키 코드값 시작
+	printf("현재 파일 위치 : %ld\n", ftell(output));
+	printf("FIEL SIZE : %d\n", size);
+	int height = size / 100;
+	size -= 100 * height; // 백자리 제외
+
+
+	// 첫 번째 배열 할당
+	ASC_column = (int**)malloc((height + 1) * sizeof(int*));
+	ASC_row = (int**)malloc((height + 1) * sizeof(int*));
+	for (int i = 0; i < height + 1; i++) {
+		ASC_column[i] = (int*)calloc(10, sizeof(int));
+		ASC_row[i] = (int*)calloc(10, sizeof(int));
+	}
+	
+	// 두 번째 배열 할당
+	compare_ASC_column = (int**)malloc((height + 1) * sizeof(int*));
+	compare_ASC_row = (int**)malloc((height + 1) * sizeof(int*));
+	for (int i = 0; i < height + 1; i++) {
+		compare_ASC_column[i] = (int*)calloc(10, sizeof(int));
+		compare_ASC_row[i] = (int*)calloc(10, sizeof(int));
+	}
+
+	// 첫 번째 배열 값 삽입
+	for (int k = 0; k < height + 1; k++) {
+		fread(ASC_column[k], sizeof(int), 10, output);
+		fread(ASC_row[k], sizeof(int), 10, output);
+	}
+	for (int k = 0; k < 10; k++) {
+		fread(ASC_height[k], sizeof(int), 10, output);
+	}
+
+
+	// 두 번째 배열 값 삽입
+	for (int k = 0; k < height + 1; k++) {
+		fread(compare_ASC_column[k], sizeof(int), 10, output);
+		fread(compare_ASC_row[k], sizeof(int), 10, output);
+	}
+	for (int k = 0; k < 10; k++) {
+		fread(compare_ASC_height[k], sizeof(int), 10, output);
+	}
+
+
+
+
+	// 값 확인
+	for (int i = 0; i < height + 1; i++) {
+		for (int j = 0; j < 10; j++) {
+			printf("ASC_column[%d][%d] : %d\n", i, j, ASC_column[i][j]);
+		}
+		printf("\n");
+	}
+	for (int i = 0; i < height + 1; i++) {
+		for (int j = 0; j < 10; j++) {
+			printf("compare_ASC_column[%d][%d] : %d\n", i, j, compare_ASC_column[i][j]);
+		}
+		printf("\n");
+	}
+
+
+	// 할당 해제
+	for (int i = 0; i < height + 1; i++) {
+		free(ASC_column[i]);
+		free(ASC_row[i]);
+	}
+	free(ASC_column);
+	free(ASC_row);
+
+	for (int i = 0; i < height + 1; i++) {
+		free(compare_ASC_column[i]);
+		free(compare_ASC_row[i]);
+	}
+	free(compare_ASC_column);
+	free(compare_ASC_row);
+	fclose(output);
+}
+void checkData() {
 	// 변조 체크를 하는 함수
 }
 
-void dataRestore(){
+void dataRestore() {
 	// 바이너리 파일 복원 함수
 }
 
 //파일 복원 함수
-void restore(const char* inputFile, const char* outputFile){
-	FILE* binaryFile;     
+void restore(const char* inputFile, const char* outputFile) {
+	FILE* binaryFile;
 	FILE* textFile;
 	// 바이너리 파일 열기
 	binaryFile = fopen(inputFile, "rb");
@@ -52,133 +156,136 @@ void restore(const char* inputFile, const char* outputFile){
 	// 바이너리 파일에서 데이터 읽어와 텍스트 파일에 쓰기
 	int ch;
 	while ((ch = fgetc(binaryFile)) != EOF) {
-    // '\n' 문자일 경우
-    if (ch == '\n') {
-        newlineCount++;
-				firstspace_check = 0;
-				if (newlineCount == 1 && userStatusCount <= 6) {
-        // *USER STATUS* 섹션 내의 데이터를 출력
-						switch (userStatusCount) {
-								case 0:
-										fprintf(textFile, "\nNAME: ");
-										break;
-								case 1:
-										fprintf(textFile, "\nGENDER: ");
-										break;
-								case 2:
-										fprintf(textFile, "\nAGE: ");
-										break;
-								case 3:
-										fprintf(textFile, "\nHP: ");
-										break;
-								case 4:
-										fprintf(textFile, "\nMP: ");
-										break;
-								case 5:
-										fprintf(textFile, "\nCOIN: ");
-										break;
-								default:
-										break;
-						}
-						userStatusCount++;
+		// '\n' 문자일 경우
+		if (ch == '\n') {
+			newlineCount++;
+			firstspace_check = 0;
+			if (newlineCount == 1 && userStatusCount <= 6) {
+				// *USER STATUS* 섹션 내의 데이터를 출력
+				switch (userStatusCount) {
+				case 0:
+					fprintf(textFile, "\nNAME: ");
+					break;
+				case 1:
+					fprintf(textFile, "\nGENDER: ");
+					break;
+				case 2:
+					fprintf(textFile, "\nAGE: ");
+					break;
+				case 3:
+					fprintf(textFile, "\nHP: ");
+					break;
+				case 4:
+					fprintf(textFile, "\nMP: ");
+					break;
+				case 5:
+					fprintf(textFile, "\nCOIN: ");
+					break;
+				default:
+					break;
 				}
-
-        if (newlineCount == 2) {
-						if(isFirstItem == 1)
-						{
-           			// 첫 번째 '\n\n'일 경우 *ITEMS* 출력
-            		fprintf(textFile, "\n*ITEMS*");
-								isFirstItem++;
-						}else if(isFirstItem == 2)
-						{
-								// 두 번째 '\n\n'일 경우 *FRIENDS LIST* 출력
-								fprintf(textFile, "\n*FRIENDS LIST*");
-								isFirstItem++;				
-						}
-        }
-    } else {
-        // 개행 문자가 아닐 경우 newlineCount 초기화
-        newlineCount = 0;
-    }
-
-    if (isFirstItem == 3) {
-        if (newlineCount == 0) {
-            if (ch >= '1' && ch <= '9' && firstspace_check == 0)
-            {
-                fprintf(textFile, "FRIEND");
-								firstspace_check++;
-            }
-        }
-    }
-
-
-		if(newlineCount==0)
-				fprintf(textFile, "%c", ch);
-    else if(userStatusCount >= 7)
-        fprintf(textFile, "%c", ch);
-    if(userStatusCount == 7)
 				userStatusCount++;
-		// *ITEMS* 섹션 이후부터 *FRIENDS LIST* 이전까지만 첫 글자를 보고 텍스트를 추가
-    if (isFirstItem == 2) {
-        if (newlineCount == 0) {
-            if (ch=='A') {
-								fseek(textFile, -1, SEEK_CUR);
-                fprintf(textFile, "BOMB:");
-            }
-            else if(ch=='P'){
-								fseek(textFile, -1, SEEK_CUR);
-                fprintf(textFile, "POTION:");
-            }
-						else if(ch=='C') {
-								fseek(textFile, -1, SEEK_CUR);
-								fprintf(textFile, "CURE:");
-						}
-						else if(ch=='B') {
-								fseek(textFile, -1, SEEK_CUR);
-								fprintf(textFile, "BOOK:");
-						}
-						else if(ch=='S') {
-								fseek(textFile, -1, SEEK_CUR);
-								fprintf(textFile, "SHIELD:");
-						}
-						else if(ch=='I') {
-								fseek(textFile, -1, SEEK_CUR);
-								fprintf(textFile, "CANNON:");
-						}
-        }
-    }
+			}
+
+			if (newlineCount == 2) {
+				if (isFirstItem == 1)
+				{
+					// 첫 번째 '\n\n'일 경우 *ITEMS* 출력
+					fprintf(textFile, "\n*ITEMS*");
+					isFirstItem++;
+				}
+				else if (isFirstItem == 2)
+				{
+					// 두 번째 '\n\n'일 경우 *FRIENDS LIST* 출력
+					fprintf(textFile, "\n*FRIENDS LIST*");
+					isFirstItem++;
+				}
+			}
+		}
+		else {
+			// 개행 문자가 아닐 경우 newlineCount 초기화
+			newlineCount = 0;
+		}
 
 		if (isFirstItem == 3) {
-        if (newlineCount == 0) {
-            if (ch == '/') {
-                fseek(textFile, -1, SEEK_CUR);
-                fprintf(textFile, "*DESCRIPTION*\n");
-                isFirstItem++;
-            }else if(ch == '&') {
-								fseek(textFile, -1, SEEK_CUR);
-								fprintf(textFile, " ");
-								switch(friend_check)
-								{
-										case 0:
-												fprintf(textFile, "ID: ");
-												friend_check++;
-												break;
-										case 1:
-												fprintf(textFile, "NAME: ");
-												friend_check++;
-												break;
-										case 2:
-												fprintf(textFile, "GENDER: ");
-												friend_check++;
-												break;
-										case 3:
-												fprintf(textFile, "AGE: ");
-												friend_check = 0;
-												break;
-								}
-						}
-        }
-    }
+			if (newlineCount == 0) {
+				if (ch >= '1' && ch <= '9' && firstspace_check == 0)
+				{
+					fprintf(textFile, "FRIEND");
+					firstspace_check++;
+				}
+			}
+		}
+
+
+		if (newlineCount == 0)
+			fprintf(textFile, "%c", ch);
+		else if (userStatusCount >= 7)
+			fprintf(textFile, "%c", ch);
+		if (userStatusCount == 7)
+			userStatusCount++;
+		// *ITEMS* 섹션 이후부터 *FRIENDS LIST* 이전까지만 첫 글자를 보고 텍스트를 추가
+		if (isFirstItem == 2) {
+			if (newlineCount == 0) {
+				if (ch == 'A') {
+					fseek(textFile, -1, SEEK_CUR);
+					fprintf(textFile, "BOMB:");
+				}
+				else if (ch == 'P') {
+					fseek(textFile, -1, SEEK_CUR);
+					fprintf(textFile, "POTION:");
+				}
+				else if (ch == 'C') {
+					fseek(textFile, -1, SEEK_CUR);
+					fprintf(textFile, "CURE:");
+				}
+				else if (ch == 'B') {
+					fseek(textFile, -1, SEEK_CUR);
+					fprintf(textFile, "BOOK:");
+				}
+				else if (ch == 'S') {
+					fseek(textFile, -1, SEEK_CUR);
+					fprintf(textFile, "SHIELD:");
+				}
+				else if (ch == 'I') {
+					fseek(textFile, -1, SEEK_CUR);
+					fprintf(textFile, "CANNON:");
+				}
+			}
+		}
+
+		if (isFirstItem == 3) {
+			if (newlineCount == 0) {
+				if (ch == '/') {
+					fseek(textFile, -1, SEEK_CUR);
+					fprintf(textFile, "*DESCRIPTION*\n");
+					isFirstItem++;
+				}
+				else if (ch == '&') {
+					fseek(textFile, -1, SEEK_CUR);
+					fprintf(textFile, " ");
+					switch (friend_check)
+					{
+					case 0:
+						fprintf(textFile, "ID: ");
+						friend_check++;
+						break;
+					case 1:
+						fprintf(textFile, "NAME: ");
+						friend_check++;
+						break;
+					case 2:
+						fprintf(textFile, "GENDER: ");
+						friend_check++;
+						break;
+					case 3:
+						fprintf(textFile, "AGE: ");
+						friend_check = 0;
+						break;
+					}
+				}
+			}
+		}
 
 	}
 
@@ -188,16 +295,15 @@ void restore(const char* inputFile, const char* outputFile){
 	printf("텍스트 파일로 변환되었습니다.\n");
 }
 
-int main(int argc, char* argv[]){
-	if (argc != 3){
+int main(int argc, char* argv[]) {
+	if (argc != 3) {
 		printf("User: ./decoder.out <input_file> <output_file>\n");
 		return 1;
 	}
 
-	const char * inputFile = argv[1];
-	const char * outputFile = argv[2];
-	restore(inputFile, outputFile);
-
+	const char* inputFile = argv[1];
+	const char* outputFile = argv[2];
+	//restore(inputFile, outputFile);
+	getTable(inputFile);
 	return 0;
 }
-
--- encode.c
+++ encode.c
@@ -2,6 +2,103 @@
 #include <stdlib.h>
 #include <string.h>
 
+void encodeASC(const char* outputFile)
+{
+    FILE* output = fopen(outputFile, "rb");
+    if (output == NULL)
+    {
+        printf("Failed to open output file.\n");
+        return;
+    }
+    int size = 0;
+    char ch;
+    while (fread(&ch, 1, 1, output) == 1) size++; // 파일 전체 사이즈
+    size -= 5;
+    printf("FIEL SIZE : %d\n", size);
+    fseek(output, 0, SEEK_SET);
+    int height = size / 100;
+
+    size -= 100 * height; // 백자리 제외
+    int** ASC_column = (int**)malloc((height + 1) * sizeof(int*));
+    int** ASC_row = (int**)malloc((height + 1) * sizeof(int*));
+    int ASC_height[10][10] = { 0 };
+    for (int i = 0; i < height + 1; i++) {
+        ASC_column[i] = (int*)calloc(10, sizeof(int));
+        ASC_row[i] = (int*)calloc(10, sizeof(int));
+    }
+    printf("동적 할당 완료\n");
+
+    for (int k = 0; k < height; k++) { // 최대 높이 만큼 반복
+        for (int i = 0; i < 10; i++) { // 세로
+            for (int j = 0; j < 10; j++) { // 가로
+                if (fread(&ch, 1, 1, output) != 1) {
+                    printf("Failed to read from output file.\n");
+                    fclose(output);
+                    return;
+                }
+                printf("%d%d%d 번째 아스키 코드값 : %d\n", k, i, j, ch);
+                ASC_column[k][j] += ch; // 높이 고정, 가로 j가 증가하면서 해당 행에 대한 값을 더한다
+                printf("ASC_column[%d][%d] : %d\n", k, j, ASC_column[k][j]);
+                ASC_row[k][i] += ch; // 높이 고정, 세로 i가 증가하면서 해당 열에 대한 값을 더한다
+                printf("ASC_row[%d][%d] : %d\n", k, i, ASC_row[k][i]);
+                ASC_height[i][j] += ch;  // 
+                printf("ASC_height[%d][%d] : %d\n\n", i, j, ASC_height[i][j]);
+            }
+        }
+    }
+    printf("100단위 값 저장 완료\n");
+
+
+    for (int i = 0; i < size / 10 + 1; i++) { // 나머지 길이만큼 반복
+        for (int j = 0; j < size % 10; j++) {
+            if (fread(&ch, 1, 1, output) != 1) {
+                printf(" Read to file end\n");
+                break;
+            }
+            printf("%d%d%d 번째 아스키 코드값 : %d\n", height, i, j, ch);
+            ASC_column[height][j] += ch; // 높이 고정, 가로 j가 증가하면서 해당 행에 대한 값을 더한다
+            printf("ASC_column[%d][%d] : %d\n", height, j, ASC_column[height][j]);
+            ASC_row[height][i] += ch; // 높이 고정, 세로 i가 증가하면서 해당 열에 대한 값을 더한다
+            printf("ASC_row[%d][%d] : %d\n", height, i, ASC_row[height][i]);
+            ASC_height[height][j] += ch;
+            printf("ASC_height[%d][%d] : %d\n\n", height, j, ASC_height[height][j]);
+        }
+    }
+    fclose(output);
+
+    // 인코딩된 값을 파일에 쓰기
+    FILE* encodedOutput = fopen(outputFile, "ab");
+    if (encodedOutput == NULL) {
+        printf("Failed to open encoded output file.\n");
+        fclose(output);
+        return;
+    }
+		printf("덧붙여 쓰기 시작 위치 : %ld\n", ftell(encodedOutput));
+    for (int k = 0; k < height + 1; k++) {
+        fwrite(ASC_column[k], sizeof(int), 10, encodedOutput);
+        fwrite(ASC_row[k], sizeof(int), 10, encodedOutput);
+    }
+    for (int k = 0; k < 10; k++) {
+        fwrite(ASC_height[k], sizeof(int), 10, encodedOutput);
+    }
+		
+    for (int k = 0; k < height + 1; k++) {
+        fwrite(ASC_column[k], sizeof(int), 10, encodedOutput);
+        fwrite(ASC_row[k], sizeof(int), 10, encodedOutput);
+    }
+    for (int k = 0; k < 10; k++) {
+        fwrite(ASC_height[k], sizeof(int), 10, encodedOutput);
+    }
+    fclose(encodedOutput);
+    // 동적 할당된 메모리 해제
+    for (int i = 0; i < height + 1; i++) {
+        free(ASC_column[i]);
+        free(ASC_row[i]);
+    }
+    free(ASC_column);
+    free(ASC_row);
+}
+
 void removeSubstring(char* s, const char* toRemove)
 {
     char* match = strstr(s, toRemove);
@@ -36,23 +133,23 @@ void encodeData(const char* inputFile, const char* outputFile)
         if (line[0] == '*')  // Check for section title
         {
             if (strcmp(line, "*USER STATUS*\n") == 0)
-						{
+            {
                 section = 0;
-						}
+            }
             else if (strcmp(line, "*ITEMS*\n") == 0)
-						{
-					  		section = 1;
-								fwrite("\n", sizeof(char), 1, output);
-						}
+            {
+                section = 1;
+                fwrite("\n", sizeof(char), 1, output);
+            }
             else if (strcmp(line, "*FRIENDS LIST*\n") == 0)
-						{
+            {
                 section = 2;
-						}
+            }
             else if (strcmp(line, "*DESCRIPTION*\n") == 0)
-						{
+            {
                 section = 3;
-								fwrite("/", sizeof(char), 1, output);
-						}
+                fwrite("/", sizeof(char), 1, output);
+            }
 
             continue;
         }
@@ -60,83 +157,91 @@ void encodeData(const char* inputFile, const char* outputFile)
         // Encode data based on section
         switch (section)
         {
-            case 0:  // User Status
-            {
-                if (strncmp(line, "ID: ", 4) == 0)
-                    fwrite(line + 4, sizeof(char), strlen(line) - 4, output);
-                else if (strncmp(line, "NAME: ", 6) == 0)
-                    fwrite(line + 6, sizeof(char), strlen(line) - 6, output);
-                else if (strncmp(line, "GENDER: ", 8) == 0)
-                    fwrite(line + 8, sizeof(char), strlen(line) - 8, output);
-                else if (strncmp(line, "AGE: ", 5) == 0)
-                    fwrite(line + 5, sizeof(char), strlen(line) - 5, output);
-                else if (strncmp(line, "HP: ", 4) == 0)
-                    fwrite(line + 4, sizeof(char), strlen(line) - 4, output);
-                else if (strncmp(line, "MP: ", 4) == 0)
-                    fwrite(line + 4, sizeof(char), strlen(line) - 4, output);
-                else if (strncmp(line, "COIN: ", 6) == 0)
-                    fwrite(line + 6, sizeof(char), strlen(line) - 6, output);
+        case 0:  // User Status
+        {
+            if (strncmp(line, "ID: ", 4) == 0)
+                fwrite(line + 4, sizeof(char), strlen(line) - 4, output);
+            else if (strncmp(line, "NAME: ", 6) == 0)
+                fwrite(line + 6, sizeof(char), strlen(line) - 6, output);
+            else if (strncmp(line, "GENDER: ", 8) == 0)
+                fwrite(line + 8, sizeof(char), strlen(line) - 8, output);
+            else if (strncmp(line, "AGE: ", 5) == 0)
+                fwrite(line + 5, sizeof(char), strlen(line) - 5, output);
+            else if (strncmp(line, "HP: ", 4) == 0)
+                fwrite(line + 4, sizeof(char), strlen(line) - 4, output);
+            else if (strncmp(line, "MP: ", 4) == 0)
+                fwrite(line + 4, sizeof(char), strlen(line) - 4, output);
+            else if (strncmp(line, "COIN: ", 6) == 0)
+                fwrite(line + 6, sizeof(char), strlen(line) - 6, output);
 
-                break;
-            }
-            case 1:  // Items
+            break;
+        }
+        case 1:  // Items
+        {
+            if (strncmp(line, "BOMB:", 5) == 0)
             {
-								if (strncmp(line, "BOMB:", 5) == 0)
-                {
-										removeSubstring(line, "BOMB:");
-										fwrite("A", sizeof(char), 1, output);										
-                }else if(strncmp(line, "POTION:", 7) == 0)
-								{
-										removeSubstring(line, "POTION:");
-										fwrite("P", sizeof(char), 1, output);
-								}else if(strncmp(line, "CURE:", 5) == 0)
-								{
-										removeSubstring(line, "CURE:");
-										fwrite("C", sizeof(char), 1, output);
-								}else if(strncmp(line, "BOOK:", 5) == 0)
-								{
-										removeSubstring(line, "BOOK:");
-										fwrite("B", sizeof(char), 1, output);
-								}else if(strncmp(line, "SHIELD:", 7) == 0)
-								{
-										removeSubstring(line, "SHIELD:");
-										fwrite("S", sizeof(char), 1, output);
-								}else if(strncmp(line, "CANNON:", 7) == 0)
-								{
-										removeSubstring(line, "CANNON:");
-										fwrite("I", sizeof(char), 1, output);
-								}
-                fwrite(line, sizeof(char), strlen(line), output);
-                break;
+                removeSubstring(line, "BOMB:");
+                fwrite("A", sizeof(char), 1, output);
             }
-            case 2:  // Friends List
+            else if (strncmp(line, "POTION:", 7) == 0)
             {
-                if (strncmp(line, "FRIEND", 6) == 0)
+                removeSubstring(line, "POTION:");
+                fwrite("P", sizeof(char), 1, output);
+            }
+            else if (strncmp(line, "CURE:", 5) == 0)
+            {
+                removeSubstring(line, "CURE:");
+                fwrite("C", sizeof(char), 1, output);
+            }
+            else if (strncmp(line, "BOOK:", 5) == 0)
+            {
+                removeSubstring(line, "BOOK:");
+                fwrite("B", sizeof(char), 1, output);
+            }
+            else if (strncmp(line, "SHIELD:", 7) == 0)
+            {
+                removeSubstring(line, "SHIELD:");
+                fwrite("S", sizeof(char), 1, output);
+            }
+            else if (strncmp(line, "CANNON:", 7) == 0)
+            {
+                removeSubstring(line, "CANNON:");
+                fwrite("I", sizeof(char), 1, output);
+            }
+            fwrite(line, sizeof(char), strlen(line), output);
+            break;
+        }
+        case 2:  // Friends List
+        {
+            if (strncmp(line, "FRIEND", 6) == 0)
+            {
+                removeSubstring(line, "FRIEND");
+                char* position = strchr(line, ' ');
+                if (position != NULL)
                 {
-										removeSubstring(line, "FRIEND");
-                    char* position = strchr(line, ' ');
-                    if (position != NULL)
-                    {
-                        removeSubstring(position, "ID: ");
-                        removeSubstring(position, "NAME: ");
-                        removeSubstring(position, "GENDER: ");
-                        removeSubstring(position, "AGE: ");
+                    removeSubstring(position, "ID: ");
+                    removeSubstring(position, "NAME: ");
+                    removeSubstring(position, "GENDER: ");
+                    removeSubstring(position, "AGE: ");
 
-												// Replace the first space with "&"
-                    		*position = '&';
-                    }
+                    // Replace the first space with "&"
+                    *position = '&';
                 }
-								fwrite(line, sizeof(char), strlen(line), output);
-                break;
             }
-            case 3:  // Description
-            {
-                fwrite(line, sizeof(char), strlen(line), output);
-                break;
-            }
+            fwrite(line, sizeof(char), strlen(line), output);
+            break;
+        }
+        case 3:  // Description
+        {
+            fwrite(line, sizeof(char), strlen(line), output);
+            break;
+        }
         }
     }
-
+    for (int i = 0; i < 5; i++) {
+        char ch = '\n';
+        fwrite(&ch, sizeof(char), 1, output);
+    }
     fclose(input);
     fclose(output);
 }
@@ -154,7 +259,7 @@ int main(int argc, char* argv[])
 
     encodeData(inputFile, outputFile);
     printf("Encoded data saved to '%s'.\n", outputFile);
-
+    encodeASC(outputFile);
+    printf("Encoded ASC saved to '%s',\n", outputFile);
     return 0;
 }
-

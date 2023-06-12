65be4da69e343ae430fdb3c840763c7e54d1899d
jjh030325 <wpqlks7@gmail.com>
2023-06-13 00:34:44 +0900

    메인으로 옮기기

--- decode.c
+++ decode.c
@@ -15,11 +15,6 @@ char* compare_compress_str;
 
 int size = 0;
 int height = 0;
-
-// (V_ASC_count, V_Char_count) = (4, 0), (3, 1), (2, 2), (1, 3), (0, 4)
-// (4, 0) 인 경우 -> 바로 해독 실행 (3, 1), (2, 2), (1, 3), (0, 4)
-// 가로 세로 높이 중 변조가 일어난 부분을 제외한 축을 이용해 변조 복구를 한다
-// 한 개의 축을 이용해서 변조 복구가 가능하다
 int V_ASC_count;
 int V_Char_count;
 
@@ -31,15 +26,56 @@ typedef struct {
 	int height;
 	char ch;
 }Char_variable; // 변조된 압축문자에 대한 정보
-
 typedef struct {
 	int xpos;
 	int ypos;
 	char type; // r = ASC_row, c = ASC_column, h = ASC_height
 }ASC_variable; // 변조된 아스키데이터에 대한 정보
-
 Char_variable* Cvar;
 ASC_variable* Avar;
+
+void getTable(const char* inputFile);
+void fileRestore(const char* inputFile, const char* outputFile);
+void checkVariable();
+char charRestore(int Cvar_num_index, int Avar_num_count);
+void dataRestore(const char* inputFile, const char* outputFile);
+
+
+int main(int argc, char* argv[]) {
+	if (argc != 3) {
+		printf("User: ./decoder.out <input_file> <output_file>\n");
+		return 1;
+	}
+
+	const char* inputFile = argv[1];
+	const char* outputFile = argv[2];
+	getTable(inputFile); // input 파일에서 데이터 전체를 문자열에 저장
+	checkVariable(); // 그 속에서 변조된 부분 찾음과 동시에 구조체 할당
+	dataRestore(inputFile, outputFile); // 변조된 부분 복원하고 input파일에 복구된 문자열을 덮어쓰기
+	fileRestore(inputFile, outputFile); // 압축된 문자열을 원본데이터로 복원
+	// 할당 해제
+	for (int i = 0; i < height + 1; i++) {
+		free(ASC_column[i]);
+		free(ASC_row[i]);
+	}
+	free(ASC_column);
+	free(ASC_row);
+
+	for (int i = 0; i < height + 1; i++) {
+		free(compare_ASC_column[i]);
+		free(compare_ASC_row[i]);
+	}
+	free(compare_ASC_column);
+	free(compare_ASC_row);
+
+	free(compress_str);
+	free(compare_compress_str);
+
+	free(Cvar);
+	free(Avar);
+
+	return 0;
+}
 
 // 아스키 배열들을 추출하기 위한 함수
 // !@#$%^ 순으로 마지막에 적힌 문자열을 고려해서
@@ -63,11 +99,11 @@ void getTable(const char* inputFile) {
 			for (int i = 0; i < 5; i++) { // 다음 글자를 읽는다 5글자
 				fread(&ch, 1, 1, output);
 				size++;
-				if (ch == '@' && i==0) symbol++;
-				else if (ch == '#' && i==1) symbol++;
-				else if (ch == '$' && i==2) symbol++;
-				else if (ch == '%' && i==3) symbol++;
-				else if (ch == '^' && i==4) symbol++;
+				if (ch == '@' && i == 0) symbol++;
+				else if (ch == '#' && i == 1) symbol++;
+				else if (ch == '$' && i == 2) symbol++;
+				else if (ch == '%' && i == 3) symbol++;
+				else if (ch == '^' && i == 4) symbol++;
 			}
 			if (symbol >= 2) {
 				printf("! 조건문 GOOD\n");
@@ -81,10 +117,10 @@ void getTable(const char* inputFile) {
 			for (int i = 0; i < 4; i++) {
 				fread(&ch, 1, 1, output);
 				size++;
-				if (ch == '#' && i==0) symbol++;
-				else if (ch == '$' && i==1) symbol++;
-				else if (ch == '%' && i==2) symbol++;
-				else if (ch == '^' && i==3) symbol++;
+				if (ch == '#' && i == 0) symbol++;
+				else if (ch == '$' && i == 1) symbol++;
+				else if (ch == '%' && i == 2) symbol++;
+				else if (ch == '^' && i == 3) symbol++;
 			}
 			if (symbol >= 2) {
 				printf("GOOD\n");
@@ -97,9 +133,9 @@ void getTable(const char* inputFile) {
 			for (int i = 0; i < 3; i++) {
 				fread(&ch, 1, 1, output);
 				size++;
-				if (ch == '$' && i==0) symbol++;
-				else if (ch == '%' && i==1) symbol++;
-				else if (ch == '^' && i==2) symbol++;
+				if (ch == '$' && i == 0) symbol++;
+				else if (ch == '%' && i == 1) symbol++;
+				else if (ch == '^' && i == 2) symbol++;
 			}
 			if (symbol >= 2) {
 				printf("GOOD\n");
@@ -112,8 +148,8 @@ void getTable(const char* inputFile) {
 			for (int i = 0; i < 2; i++) {
 				fread(&ch, 1, 1, output);
 				size++;
-				if (ch == '%' && i==0) symbol++;
-				else if (ch == '^' && i==1) symbol++;
+				if (ch == '%' && i == 0) symbol++;
+				else if (ch == '^' && i == 1) symbol++;
 			}
 			if (symbol >= 2) {
 				printf("GOOD\n");
@@ -180,8 +216,8 @@ void getTable(const char* inputFile) {
 	compress_str[size] = '\n';
 	compare_compress_str[size] = '\n';
 	// 값 확인
-	
-	
+
+
 	for (int i = 0; i < height + 1; i++) {
 		for (int j = 0; j < 10; j++) {
 			printf("ASC_column[%d][%d] : %d\n", i, j, ASC_column[i][j]);
@@ -194,15 +230,17 @@ void getTable(const char* inputFile) {
 		}
 		printf("\n");
 	}
-	
-	
+
+
 	printf("compress_str : %s\n", compress_str);
 	printf("compare_compress_str : %s\n", compare_compress_str);
 	//printf("getTable 성공\n");
 	fclose(output);
 	output = NULL;
 }
-void checkData() {
+
+
+void checkVariable() {
 	// 압축된 문자열에서 변조 확인 확인 
 	Cvar = (Char_variable*)malloc(sizeof(Char_variable));
 	Avar = (ASC_variable*)malloc(sizeof(ASC_variable));
@@ -211,7 +249,7 @@ void checkData() {
 
 	for (int i = 0; i < size; i++) {
 		if (compare_compress_str[i] != compress_str[i]) {
-			if(V_Char_count != 0) Cvar = (Char_variable*)realloc(Cvar, sizeof(Char_variable) * (V_Char_count+1));
+			if (V_Char_count != 0) Cvar = (Char_variable*)realloc(Cvar, sizeof(Char_variable) * (V_Char_count + 1));
 			(Cvar + V_Char_count)->ch = compress_str[i];
 			(Cvar + V_Char_count)->height = i / 100;
 			for (int j = 0; j < height + 1; j++) {
@@ -278,13 +316,155 @@ void checkData() {
 	}
 }
 
-//압축된 문자 복원 함수
-void dataRestore() {
-	if()
+// 문자 복구 과정
+// Cvar_num_index = Cvar 구조체 번호, Avar_num_count = Avar 구조체 갯수
+char charRestore(int Cvar_num_index, int Avar_num_count) {
+	char restore_ch = Cvar[Cvar_num_index].ch;
+	int sum = 0;
+
+	if (Avar_num_count == 0) {
+		if (Cvar[Cvar_num_index].row == Cvar[(Cvar_num_index + 1) % 4].row || // 변조된 변수가 같은 행에 있을 경우 조건문이 참
+			Cvar[Cvar_num_index].row == Cvar[(Cvar_num_index + 2) % 4].row ||
+			Cvar[Cvar_num_index].row == Cvar[(Cvar_num_index + 3) % 4].row) {
+			if (Cvar[Cvar_num_index].column == Cvar[(Cvar_num_index + 1) % 4].column || // 변조된 변수가 같은 열에 있을 경우 조건문이 참
+				Cvar[Cvar_num_index].column == Cvar[(Cvar_num_index + 2) % 4].column ||
+				Cvar[Cvar_num_index].column == Cvar[(Cvar_num_index + 3) % 4].column) {
+				if (Cvar[Cvar_num_index].height == Cvar[(Cvar_num_index + 1) % 4].height || // 변조된 변수가 같은 높이에 있을 경우 조건문이 참
+					Cvar[Cvar_num_index].height == Cvar[(Cvar_num_index + 2) % 4].height ||
+					Cvar[Cvar_num_index].height == Cvar[(Cvar_num_index + 3) % 4].height) {
+					fprintf(stderr, "미친 확률 발생\n");
+					exit(1);
+				}
+				else {
+					for (int i = 10 * Cvar[Cvar_num_index].row + Cvar[Cvar_num_index].column; i < height + 1; i += 100) {
+						sum += compress_str[i];
+					}
+					restore_ch = Cvar[Cvar_num_index].ch + ASC_height[Cvar[Cvar_num_index].row][Cvar[Cvar_num_index].column] - sum;
+				}
+			}
+			else {
+				for (int i = 100 * Cvar[Cvar_num_index].height + Cvar[Cvar_num_index].column; i < 100 * Cvar[Cvar_num_index].height + Cvar[Cvar_num_index].column + 10; i += 10) {
+					sum += compress_str[i];
+				}
+				restore_ch = Cvar[Cvar_num_index].ch + ASC_column[Cvar[Cvar_num_index].height][Cvar[Cvar_num_index].column] - sum;
+			}
+		}
+		else {
+			for (int i = 100 * Cvar[Cvar_num_index].height + 10 * Cvar[Cvar_num_index].row; i < 100 * Cvar[Cvar_num_index].height + 10 * Cvar[Cvar_num_index].row + 10; i++) {
+				sum += compress_str[i];
+			}
+			restore_ch = Cvar[Cvar_num_index].ch + ASC_row[Cvar[Cvar_num_index].height][Cvar[Cvar_num_index].row] - sum;
+		}
+	}
+	else if (Avar_num_count == 1) {
+		if (Cvar[Cvar_num_index].row == Cvar[(Cvar_num_index + 1) % 3].row ||
+			Cvar[Cvar_num_index].row == Cvar[(Cvar_num_index + 2) % 3].row ||
+			(Cvar[Cvar_num_index].height == Avar[0].ypos && Cvar[Cvar_num_index].row == Avar[0].xpos)) {
+			if (Cvar[Cvar_num_index].column == Cvar[(Cvar_num_index + 1) % 3].column ||
+				Cvar[Cvar_num_index].column == Cvar[(Cvar_num_index + 2) % 3].column ||
+				(Cvar[Cvar_num_index].height == Avar[0].ypos && Cvar[Cvar_num_index].column == Avar[0].xpos)) {
+				if (Cvar[Cvar_num_index].height == Cvar[(Cvar_num_index + 1) % 3].height ||
+					Cvar[Cvar_num_index].height == Cvar[(Cvar_num_index + 2) % 3].height ||
+					(Cvar[Cvar_num_index].row == Avar[0].ypos && Cvar[Cvar_num_index].column == Avar[0].xpos)) {
+					fprintf(stderr, "미친 확률 발생\n");
+					exit(1);
+				}
+				else { // ASC_heigh 이용해서 값 산출
+					for (int i = 10 * Cvar[Cvar_num_index].row + Cvar[Cvar_num_index].column; i < height + 1; i += 100) {
+						sum += compress_str[i];
+					}
+					restore_ch = Cvar[Cvar_num_index].ch + ASC_height[Cvar[Cvar_num_index].row][Cvar[Cvar_num_index].column] - sum;
+				}
+			}
+			else { // ASC_column 이용해서 값 산출
+				for (int i = 100 * Cvar[Cvar_num_index].height + Cvar[Cvar_num_index].column; i < 100 * Cvar[Cvar_num_index].height + Cvar[Cvar_num_index].column + 10; i += 10) {
+					sum += compress_str[i];
+				}
+				restore_ch = Cvar[Cvar_num_index].ch + ASC_column[Cvar[Cvar_num_index].height][Cvar[Cvar_num_index].column] - sum;
+			}
+		}
+		else { // ASC_row 이용해서 값 산출
+			for (int i = 100 * Cvar[Cvar_num_index].height + 10 * Cvar[Cvar_num_index].row; i < 100 * Cvar[Cvar_num_index].height + 10 * Cvar[Cvar_num_index].row + 10; i++) {
+				sum += compress_str[i];
+			}
+			restore_ch = Cvar[Cvar_num_index].ch + ASC_row[Cvar[Cvar_num_index].height][Cvar[Cvar_num_index].row] - sum;
+		}
+	}
+	else if (Avar_num_count == 2) {
+		if (Cvar[Cvar_num_index].row == Cvar[(Cvar_num_index + 1) % 2].row ||
+			(Cvar[Cvar_num_index].height == Avar[0].ypos && Cvar[Cvar_num_index].row == Avar[0].xpos) ||
+			(Cvar[Cvar_num_index].height == Avar[1].ypos && Cvar[Cvar_num_index].row == Avar[1].xpos)) {
+			if (Cvar[Cvar_num_index].column == Cvar[(Cvar_num_index + 1) % 2].column ||
+				(Cvar[Cvar_num_index].height == Avar[0].ypos && Cvar[Cvar_num_index].column == Avar[0].xpos) ||
+				(Cvar[Cvar_num_index].height == Avar[1].ypos && Cvar[Cvar_num_index].column == Avar[1].xpos)) {
+				if (Cvar[Cvar_num_index].height == Cvar[(Cvar_num_index + 1) % 2].height ||
+					(Cvar[Cvar_num_index].row == Avar[0].ypos && Cvar[Cvar_num_index].column == Avar[0].xpos) ||
+					(Cvar[Cvar_num_index].row == Avar[1].ypos && Cvar[Cvar_num_index].column == Avar[1].xpos)) {
+					fprintf(stderr, "미친 확률 발생\n");
+					exit(1);
+				}
+				else {
+					for (int i = 10 * Cvar[Cvar_num_index].row + Cvar[Cvar_num_index].column; i < height + 1; i += 100) {
+						sum += compress_str[i];
+					}
+					restore_ch = Cvar[Cvar_num_index].ch + ASC_height[Cvar[Cvar_num_index].row][Cvar[Cvar_num_index].column] - sum;
+				}
+			}
+			else {
+				for (int i = 100 * Cvar[Cvar_num_index].height + Cvar[Cvar_num_index].column; i < 100 * Cvar[Cvar_num_index].height + Cvar[Cvar_num_index].column + 10; i += 10) {
+					sum += compress_str[i];
+				}
+				restore_ch = Cvar[Cvar_num_index].ch + ASC_column[Cvar[Cvar_num_index].height][Cvar[Cvar_num_index].column] - sum;
+			}
+		}
+		else {
+			for (int i = 100 * Cvar[Cvar_num_index].height + 10 * Cvar[Cvar_num_index].row; i < 100 * Cvar[Cvar_num_index].height + 10 * Cvar[Cvar_num_index].row + 10; i++) {
+				sum += compress_str[i];
+			}
+			restore_ch = Cvar[Cvar_num_index].ch + ASC_row[Cvar[Cvar_num_index].height][Cvar[Cvar_num_index].row] - sum;
+		}
+	}
+	else {
+		if ((Cvar[Cvar_num_index].height == Avar[0].ypos && Cvar[Cvar_num_index].row == Avar[0].xpos) ||
+			(Cvar[Cvar_num_index].height == Avar[1].ypos && Cvar[Cvar_num_index].row == Avar[1].xpos) ||
+			(Cvar[Cvar_num_index].height == Avar[2].ypos && Cvar[Cvar_num_index].row == Avar[2].xpos)) {
+			if ((Cvar[Cvar_num_index].height == Avar[0].ypos && Cvar[Cvar_num_index].column == Avar[0].xpos) ||
+				(Cvar[Cvar_num_index].height == Avar[1].ypos && Cvar[Cvar_num_index].column == Avar[1].xpos) ||
+				(Cvar[Cvar_num_index].height == Avar[2].ypos && Cvar[Cvar_num_index].column == Avar[2].xpos)) {
+				if ((Cvar[Cvar_num_index].row == Avar[0].ypos && Cvar[Cvar_num_index].column == Avar[0].xpos) ||
+					(Cvar[Cvar_num_index].row == Avar[1].ypos && Cvar[Cvar_num_index].column == Avar[1].xpos) ||
+					(Cvar[Cvar_num_index].row == Avar[2].ypos && Cvar[Cvar_num_index].column == Avar[2].xpos)) {
+					fprintf(stderr, "미친 확률 발생\n");
+					exit(1);
+				}
+				else {
+					for (int i = 10 * Cvar[Cvar_num_index].row + Cvar[Cvar_num_index].column; i < height + 1; i += 100) {
+						sum += compress_str[i];
+					}
+					restore_ch = Cvar[Cvar_num_index].ch + ASC_height[Cvar[Cvar_num_index].row][Cvar[Cvar_num_index].column] - sum;
+				}
+			}
+			else {
+				for (int i = 100 * Cvar[Cvar_num_index].height + Cvar[Cvar_num_index].column; i < 100 * Cvar[Cvar_num_index].height + Cvar[Cvar_num_index].column + 10; i += 10) {
+					sum += compress_str[i];
+				}
+				restore_ch = Cvar[Cvar_num_index].ch + ASC_column[Cvar[Cvar_num_index].height][Cvar[Cvar_num_index].column] - sum;
+			}
+		}
+		else {
+			for (int i = 100 * Cvar[Cvar_num_index].height + 10 * Cvar[Cvar_num_index].row; i < 100 * Cvar[Cvar_num_index].height + 10 * Cvar[Cvar_num_index].row + 10; i++) {
+				sum += compress_str[i];
+			}
+			restore_ch = Cvar[Cvar_num_index].ch + ASC_row[Cvar[Cvar_num_index].height][Cvar[Cvar_num_index].row] - sum;
+		}
+	}
+
+	return restore_ch;
 }
 
+
 //파일 복원 함수
-void restore(const char* inputFile, const char* outputFile) {
+void fileRestore(const char* inputFile, const char* outputFile) {
 	FILE* binaryFile;
 	FILE* textFile;
 	// 바이너리 파일 열기
@@ -312,7 +492,8 @@ void restore(const char* inputFile, const char* outputFile) {
 
 	// 바이너리 파일에서 데이터 읽어와 텍스트 파일에 쓰기
 	int ch;
-	while ((ch = fgetc(binaryFile)) != EOF) {
+	for (int i = 0; i < size; i++ ) {
+		ch = compress_str[i];
 		// '\n' 문자일 경우
 		if (ch == '\n') {
 			newlineCount++;
@@ -452,38 +633,61 @@ void restore(const char* inputFile, const char* outputFile) {
 	printf("텍스트 파일로 변환되었습니다.\n");
 }
 
-int main(int argc, char* argv[]) {
-	if (argc != 3) {
-		printf("User: ./decoder.out <input_file> <output_file>\n");
-		return 1;
+
+//  Avar -> type, xpos, ypos       Cvar -> ch, row, column, height
+// (V_ASC_count, V_Char_count) = (4, 0), (3, 1), (2, 2), (1, 3), (0, 4)
+// (4, 0) 인 경우 -> 바로 디코딩 실행 (3, 1), (2, 2), (1, 3), (0, 4)
+// 가로 세로 높이 중 변조가 일어난 부분을 제외한 축을 이용해 변조 복구를 한다
+// 한 개의 축을 이용해서 변조 복구가 가능하다
+// 압축된 문자 복원 함수
+void dataRestore(const char* inputFile, const char* outputFile) {
+	printf("dataRestore 진입\n");
+	if (V_ASC_count == 4) {
+		printf("V_ASC_count == 4 진입\n");
+	}
+	else if (V_ASC_count == 3 && V_Char_count == 1) {
+		//Avar[0]; Avar[1]; Avar[2]; Cvar[0];
+		char restore_ch0 = charRestore(0, 3);
+
+
+		compress_str[Cvar[0].height * 100 + Cvar[0].row * 10 + Cvar[0].column] = restore_ch0; // 복구
+	}
+	else if (V_ASC_count == 2 && V_Char_count == 2) {
+		//Avar[0]; Avar[1]; Cvar[0]; Cvar[1];
+		char restore_ch0 = charRestore(0, 2);
+		char restore_ch1 = charRestore(1, 2);
+
+
+		compress_str[Cvar[0].height * 100 + Cvar[0].row * 10 + Cvar[0].column] = restore_ch0;
+		compress_str[Cvar[1].height * 100 + Cvar[1].row * 10 + Cvar[1].column] = restore_ch1;
+	}
+	else if (V_ASC_count == 1 && V_Char_count == 3) {
+		//Avar[0]; Cvar[0]; Cvar[1]; Cvar[2];
+		char restore_ch0 = charRestore(0, 1);
+		char restore_ch1 = charRestore(1, 1);
+		char restore_ch2 = charRestore(2, 1);
+
+
+		compress_str[Cvar[0].height * 100 + Cvar[0].row * 10 + Cvar[0].column] = restore_ch0;
+		compress_str[Cvar[1].height * 100 + Cvar[1].row * 10 + Cvar[1].column] = restore_ch1;
+		compress_str[Cvar[2].height * 100 + Cvar[2].row * 10 + Cvar[2].column] = restore_ch2;
+	}
+	else {
+		//Cvar[0]; Cvar[1]; Cvar[2]; Cvar[3];
+		char restore_ch0 = charRestore(0, 0);
+		char restore_ch1 = charRestore(1, 0);
+		char restore_ch2 = charRestore(2, 0);
+		char restore_ch3 = charRestore(3, 0);
+
+
+		compress_str[Cvar[0].height * 100 + Cvar[0].row * 10 + Cvar[0].column] = restore_ch0;
+		compress_str[Cvar[1].height * 100 + Cvar[1].row * 10 + Cvar[1].column] = restore_ch1;
+		compress_str[Cvar[2].height * 100 + Cvar[2].row * 10 + Cvar[2].column] = restore_ch2;
+		compress_str[Cvar[3].height * 100 + Cvar[3].row * 10 + Cvar[3].column] = restore_ch3;
 	}
 
-	const char* inputFile = argv[1];
-	const char* outputFile = argv[2];
-	restore(inputFile, outputFile);
-	getTable(inputFile);
-	checkData();
+	// 복구된 문자열 파일에 덮어쓰기
 
-	// 할당 해제
-	for (int i = 0; i < height + 1; i++) {
-		free(ASC_column[i]);
-		free(ASC_row[i]);
-	}
-	free(ASC_column);
-	free(ASC_row);
-
-	for (int i = 0; i < height + 1; i++) {
-		free(compare_ASC_column[i]);
-		free(compare_ASC_row[i]);
-	}
-	free(compare_ASC_column);
-	free(compare_ASC_row);
-
-	free(compress_str);
-	free(compare_compress_str);
-
-	free(Cvar);
-	free(Avar);
-
-	return 0;
 }
+
+
